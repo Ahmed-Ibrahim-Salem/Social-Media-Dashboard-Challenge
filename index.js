09f4076129f911249794be4c46dd5fcf7fdbcb81
Ahmed-Ibrahim-Salem <72328005+Ahmed-Ibrahim-Salem@users.noreply.github.com>
2022-02-14 21:15:29 +0200

    add dark mode functionality

---
+++ index.js
@@ -0,0 +1,18 @@
+let toggle = document.getElementById("theme-toggle");
+
+let storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+if (storedTheme)
+    document.documentElement.setAttribute('data-theme', storedTheme)
+
+
+toggle.onclick = function () {
+    let currentTheme = document.documentElement.getAttribute("data-theme");
+    let targetTheme = "dark";
+
+    if (currentTheme === "dark") {
+        targetTheme = "light";
+    }
+
+    document.documentElement.setAttribute('data-theme', targetTheme)
+    localStorage.setItem('theme', targetTheme);
+};
